7e979c0b01ece4aa6597f1ccccc0d7a1d92142c1
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-04-24 17:56:52 +0200

    Connected react and redux

--- React-todo-with-redux/src/TodoList.jsx
+++ React-todo-with-redux/src/TodoList.jsx
@@ -5,11 +5,39 @@ import { connect } from 'react-redux';
 class TodoList extends Component {
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            task: ""
+        }
     }
+
+    handleSubmit(e){
+        e.preventDefault()
+        this.props.dispatch({
+            type: 'ADD_TODO',
+            task: this.state.task
+        })
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render(){
-        let todos = this.state.todos.map((todo, index) => <Todo task={todo} key={index}/>)
+        let todos = this.props.todos.map((todo, index) => <Todo task={todo} key={index}/>)
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="task"> Task </label>
+                    <input type="text"
+                            name='task'
+                            id='task' 
+                            onChange={this.handleChange}/>
+                    <button>Add a Todo</button>
+                </form>
                 <ul>
                     {todos}
                 </ul>
@@ -18,4 +46,10 @@ class TodoList extends Component {
     }
 }
 
-export default connect()(TodoList)
+function mapStateToProps(reduxState) {
+    return {
+        todos: reduxState.todos
+    }
+}
+
+export default connect(mapStateToProps)(TodoList)
