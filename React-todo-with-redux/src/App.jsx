1f55a365a81a464810fa70f3ca48e99ac634d1ac
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-04-25 07:27:55 +0200

    added in React Router and basic Links and Routes

--- React-todo-with-redux/src/App.jsx
+++ React-todo-with-redux/src/App.jsx
@@ -1,11 +1,19 @@
 import TodoList from './TodoList'
+import {Link, Route, Redirect} from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
       <h1>See Our Todos!</h1>
-      <TodoList />
+      <p>
+        <Link to="/todos">See my todos</Link>
+      </p>
+      <p>
+        <Link to="/todos/new">Add a todos</Link>
+      </p>
+      <Route path="/todos" component = {TodoList}/>
+      <Route path="/" render={()=> <Redirect to="/todos" />}/>
     </div>
   )
 }
--- React-todo-with-redux/src/main.jsx
+++ React-todo-with-redux/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 import {legacy_createStore} from 'redux'
 import rootReducer from './rootReducer'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 const store = legacy_createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 )
