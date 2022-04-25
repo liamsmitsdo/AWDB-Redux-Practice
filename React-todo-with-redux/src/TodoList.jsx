5751fd5417197bde109f6ba34e7954dd362c3436
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-04-25 07:21:25 +0200

    added in mapDispatchToProps functionality

--- React-todo-with-redux/src/TodoList.jsx
+++ React-todo-with-redux/src/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Todo from './Todo'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
+import { addTodo, removeTodo } from './actions'
 
 class TodoList extends Component {
     constructor(props) {
@@ -15,10 +16,7 @@ class TodoList extends Component {
 
     handleSubmit(e){
         e.preventDefault()
-        this.props.dispatch({
-            type: 'ADD_TODO',
-            task: this.state.task
-        })
+        this.props.addTodo(this.state.task)
         e.target.reset()
     }
 
@@ -29,10 +27,7 @@ class TodoList extends Component {
     }
 
     removeTodo(id, event){
-        this.props.dispatch({
-            type: "REMOVE_TODO",
-            id
-        })
+        this.props.removeTodo(id)
     }
 
     render(){
@@ -61,4 +56,4 @@ function mapStateToProps(reduxState) {
     }
 }
 
-export default connect(mapStateToProps)(TodoList)
+export default connect(mapStateToProps, { addTodo, removeTodo })(TodoList)
